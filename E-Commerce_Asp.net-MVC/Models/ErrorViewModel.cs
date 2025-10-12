153a745b869059651938bd7757d73b32a51f53a9
Yelda <yeldakuru99@outlook.com>
2025-10-12 21:39:56 +0300

    Add project files.

---
+++ E-Commerce_Asp.net-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_Commerce_Asp.net_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
