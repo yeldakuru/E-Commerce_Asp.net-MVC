2acec545f1aa3bf1f202669b14bfc58e7a412684
Yelda <yeldakuru99@outlook.com>
2025-10-17 18:50:18 +0300

    bug fixed,product and slider done

---
+++ E-Commerce_Asp.net-MVC/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,169 @@
+﻿using ECommerce.Core.Entities;
+using ECommerce.Data;
+using ECommerce_UI.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce_UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public ProductsController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Products
+        public async Task<IActionResult> Index()
+        {
+            var databaseContext = _context.Products.Include(p => p.Brand).Include(p => p.Category);
+            return View(await databaseContext.ToListAsync());
+        }
+
+        // GET: Admin/Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Brand)
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Admin/Products/Create
+        public IActionResult Create()
+        {
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Name");
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+            return View();
+        }
+
+        // POST: Admin/Products/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product, IFormFile? Image)
+        {
+            if (ModelState.IsValid)
+            {
+                product.Image = await FileHelper.FileLoaderAsync(Image, "/Img/Products/");
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Name", product.BrandId);
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        // GET: Admin/Products/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Name", product.BrandId);
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        // POST: Admin/Products/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Product product, IFormFile? Image, bool cbDeleteImage = false)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (cbDeleteImage)
+                        product.Image = string.Empty;
+                    if (Image is not null)
+                        product.Image = await FileHelper.FileLoaderAsync(Image, "/Img/Products/");
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "Name", product.BrandId);
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+            return View(product);
+        }
+
+        // GET: Admin/Products/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Brand)
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Admin/Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ E-Commerce_Asp.net-MVC/Areas/Admin/Controllers/SlidersController.cs
@@ -0,0 +1,160 @@
+﻿using ECommerce.Core.Entities;
+using ECommerce.Data;
+using ECommerce_UI.Utils;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ECommerce_UI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SlidersController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public SlidersController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/Sliders
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Sliders.ToListAsync());
+        }
+
+        // GET: Admin/Sliders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var slider = await _context.Sliders
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (slider == null)
+            {
+                return NotFound();
+            }
+
+            return View(slider);
+        }
+
+        // GET: Admin/Sliders/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Sliders/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Slider slider, IFormFile? Image)
+        {
+            if (ModelState.IsValid)
+            {
+                slider.Image = await FileHelper.FileLoaderAsync(Image, "/Img/Slider/");
+                _context.Add(slider);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(slider);
+        }
+
+        // GET: Admin/Sliders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var slider = await _context.Sliders.FindAsync(id);
+            if (slider == null)
+            {
+                return NotFound();
+            }
+            return View(slider);
+        }
+
+        // POST: Admin/Sliders/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Slider slider, IFormFile? Image, bool cbDeleteImage= false)
+        {
+            if (id != slider.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (cbDeleteImage)
+                        slider.Image = string.Empty;
+                    if (Image is not null)
+                        slider.Image = await FileHelper.FileLoaderAsync(Image, "/Img/Slider/");
+                    _context.Update(slider);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SliderExists(slider.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(slider);
+        }
+
+        // GET: Admin/Sliders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var slider = await _context.Sliders
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (slider == null)
+            {
+                return NotFound();
+            }
+
+            return View(slider);
+        }
+
+        // POST: Admin/Sliders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var slider = await _context.Sliders.FindAsync(id);
+            if (slider != null)
+            {
+                if (!string.IsNullOrEmpty(slider.Image))
+                {
+                    FileHelper.FileRemover(slider.Image, "/Img/Slider/");
+                }
+                _context.Sliders.Remove(slider);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SliderExists(int id)
+        {
+            return _context.Sliders.Any(e => e.Id == id);
+        }
+    }
+}
