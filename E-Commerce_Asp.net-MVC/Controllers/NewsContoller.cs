5a9afc3611eb09474e4e4de949239954aa4c98e0
Yelda Kuru <yeldakuru99@outlook.com>
2025-10-20 17:58:29 +0300

    Campaign detail pages created

---
+++ E-Commerce_Asp.net-MVC/Controllers/NewsContoller.cs
@@ -0,0 +1,38 @@
+﻿using ECommerce.Core.Entities;
+using ECommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce_UI.Controllers
+{
+    public class NewsController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public NewsController(DatabaseContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.News.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var news = await _context.News
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (news == null)
+            {
+                return NotFound();
+            }
+
+            return View(news);
+        }
+    }
+}
