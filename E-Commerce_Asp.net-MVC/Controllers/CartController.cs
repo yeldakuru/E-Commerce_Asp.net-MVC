8b3bc9b0899fe26462894f93e5c794fe980c89be
Yelda Kuru <yeldakuru99@outlook.com>
2025-11-16 17:53:40 +0300

    virtual credit card integration

--- E-Commerce_Asp.net-MVC/Controllers/CartController.cs
+++ E-Commerce_Asp.net-MVC/Controllers/CartController.cs
@@ -3,25 +3,32 @@ using ECommerce.Service.Abstract;
 using ECommerce.Service.Concrete;
 using ECommerce_UI.ExtensionMethods;
 using ECommerce_UI.Models;
+using Iyzipay;
+using Iyzipay.Model;
+using Iyzipay.Request;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using System;
+using System.Configuration;
 
 namespace ECommerce_UI.Controllers
 {
     public class CartController : Controller
     {
         private readonly IService<Product> _serviceProduct;
-        private readonly IService<Address> _serviceAddress;
+        private readonly IService<ECommerce.Core.Entities.Address> _serviceAddress;
         private readonly IService<AppUser> _serviceAppUser;
         private readonly IService<Order> _serviceOrder;
-        public CartController(IService<Product> serviceProduct, IService<Address> serviceAddress, IService<AppUser> serviceAppUser, IService<Order> serviceOrder)
+        private readonly IConfiguration _configuration;
+        public CartController(IService<Product> serviceProduct, IService<ECommerce.Core.Entities.Address> serviceAddress, IService<AppUser> serviceAppUser, IService<Order> serviceOrder, IConfiguration configuration)
         {
             _serviceProduct = serviceProduct;
             _serviceAddress = serviceAddress;
             _serviceAppUser = serviceAppUser;
             _serviceOrder = serviceOrder;
+            _configuration = configuration;
         }
         public IActionResult Index()
         {
@@ -88,6 +95,7 @@ namespace ECommerce_UI.Controllers
             };
             return View(model);
         }
+        [Authorize, HttpPost]
         public async Task<IActionResult> Checkout(string CardNameSurname, string CardNumber, string CardMonth, string CardYear, string CVV, string DeliveryAddress, string BillingAddress)
         {
             var cart = GetCart();
@@ -122,6 +130,77 @@ namespace ECommerce_UI.Controllers
                 OrderState = 0,
                 OrderLines = []
             };
+
+            // Ödeme çekme
+            #region OdemeIslemi
+            Iyzipay.Options options = new Iyzipay.Options();
+            options.ApiKey = _configuration["IyzicOptions:ApiKey"];
+            options.SecretKey = _configuration["IyzicOptions:SecretKey"];
+            options.BaseUrl = _configuration["IyzicOptions:BaseUrl"]; //"https://sandbox-api.iyzipay.com";
+
+            CreatePaymentRequest request = new CreatePaymentRequest();
+            request.Locale = Locale.TR.ToString();
+            request.ConversationId = HttpContext.Session.Id;
+            request.Price = siparis.TotalPrice.ToString().Replace(",", ".");
+            request.PaidPrice = siparis.TotalPrice.ToString().Replace(",", ".");
+            request.Currency = Currency.TRY.ToString();
+            request.Installment = 1;
+            request.BasketId = "B" + HttpContext.Session.Id;
+            request.PaymentChannel = PaymentChannel.WEB.ToString();
+            request.PaymentGroup = PaymentGroup.PRODUCT.ToString();
+
+            PaymentCard paymentCard = new PaymentCard();
+            paymentCard.CardHolderName = CardNameSurname; // "John Doe";
+            paymentCard.CardNumber = CardNumber; // "5528790000000008";
+            paymentCard.ExpireMonth = CardMonth; // "12";
+            paymentCard.ExpireYear = CardYear; // "2030";
+            paymentCard.Cvc = CVV; // "123";
+            paymentCard.RegisterCard = 0;
+            request.PaymentCard = paymentCard;
+
+            Buyer buyer = new Buyer();
+            buyer.Id = "BY" + appUser.Id;
+            buyer.Name = appUser.Name;
+            buyer.Surname = appUser.Surname;
+            buyer.GsmNumber = appUser.Phone;
+            buyer.Email = appUser.Email;
+            buyer.IdentityNumber = "11111111111";
+            buyer.LastLoginDate = DateTime.Now.ToString("yyyy-mm-dd hh:mm:ss"); // "2015-10-05 12:43:35";
+            buyer.RegistrationDate = appUser.CreateDate.ToString("yyyy-mm-dd hh:mm:ss"); // "2013-04-21 15:12:09";
+            buyer.RegistrationAddress = siparis.DeliveryAddress;
+            buyer.Ip = HttpContext.Connection.RemoteIpAddress?.ToString(); // "85.34.78.112";
+            buyer.City = teslimatAdresi.City;
+            buyer.Country = "Turkey";
+            buyer.ZipCode = "34732";
+            request.Buyer = buyer;
+
+            var shippingAddress = new Iyzipay.Model.Address();
+            shippingAddress.ContactName = appUser.Name + " " + appUser.Surname;
+            shippingAddress.City = teslimatAdresi.City;
+            shippingAddress.Country = "Turkey";
+            shippingAddress.Description = teslimatAdresi.OpenAddress;
+            shippingAddress.ZipCode = "34742";
+            request.ShippingAddress = shippingAddress;
+
+            var billingAddress = new Iyzipay.Model.Address();
+            billingAddress.ContactName = appUser.Name + " " + appUser.Surname;
+            billingAddress.City = faturaAdresi.City;
+            billingAddress.Country = "Turkey";
+            billingAddress.Description = faturaAdresi.OpenAddress;
+            billingAddress.ZipCode = "34742";
+            request.BillingAddress = billingAddress;
+
+            List<BasketItem> basketItems = new List<BasketItem>();
+
+            //BasketItem firstBasketItem = new BasketItem();
+            //firstBasketItem.Id = "BI101";
+            //firstBasketItem.Name = "Binocular";
+            //firstBasketItem.Category1 = "Collectibles";
+            //firstBasketItem.Category2 = "Accessories";
+            //firstBasketItem.ItemType = BasketItemType.PHYSICAL.ToString();
+            //firstBasketItem.Price = "0.3";
+            //basketItems.Add(firstBasketItem);
+
             foreach (var item in cart.CartLines)
             {
                 siparis.OrderLines.Add(new OrderLine
@@ -131,8 +210,44 @@ namespace ECommerce_UI.Controllers
                     Quantity = item.Quantity,
                     UnitPrice = item.Product.Price
                 });
-
+                basketItems.Add(new BasketItem
+                {
+                    Id = item.Product.Id.ToString(),
+                    Name = item.Product.Name,
+                    Category1 = "Collectibles",
+                    ItemType = BasketItemType.PHYSICAL.ToString(),
+                    Price = (item.Product.Price * item.Quantity).ToString().Replace(",", ".")
+                });
             }
+
+            request.BasketItems = basketItems;
+
+            Payment payment = await Payment.Create(request, options);
+
+            #endregion
+            try
+            {
+                if (payment.Status == "success")
+                {
+                    // sipariş oluştur
+                    await _serviceOrder.AddAsync(siparis);
+                    var sonuc = await _serviceOrder.SaveChangesAsync();
+                    if (sonuc > 0)
+                    {
+                        HttpContext.Session.Remove("Cart");
+                        return RedirectToAction("Thanks");
+                    }
+                }
+                else
+                {
+                    TempData["Message"] = $"<div class='alert alert-danger'>Ödeme İşlemi Başarısız!</div> ({payment.ErrorMessage})";
+                }
+            }
+            catch (Exception)
+            {
+                TempData["Message"] = "<div class='alert alert-danger'>Hata Oluştu!</div>";
+            }
+
             return View(model);
         }
         public IActionResult Thanks()
